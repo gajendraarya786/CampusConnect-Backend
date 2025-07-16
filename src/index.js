f503b4fee8fdcc2a91d6bef0b2f55a24bd3f0839
Gajendra Kumar Arya <gajendra.work538@gmail.com>
2025-07-16 14:57:32 +0530

    updated cloudinary url

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import dotenv from 'dotenv'
 dotenv.config({
     path: './.env'
 })
+const PORT = process.env.PORT || 8000;
 
 import connectDB from "./db/db.js";
 import { app } from "./app.js";
@@ -15,8 +16,8 @@ setupSocket(server);
 
 connectDB()
 .then(() => {
-    server.listen(process.env.PORT || 8000, () => {
-        console.log(`Server is running on port ${process.env.PORT}`);
+    server.listen(PORT || 8000, () => {
+        console.log(`Server is running on port ${PORT}`);
     })
 })
 .catch((err) => {
