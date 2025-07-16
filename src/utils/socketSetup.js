1eabd9e6e51ae42481b048ffea85ddfaf0a0fd43
Gajendra Kumar Arya <gajendra.work538@gmail.com>
2025-07-16 15:07:50 +0530

    updated socketSetup

--- src/utils/socketSetup.js
+++ src/utils/socketSetup.js
@@ -11,11 +11,13 @@ function setupSocket(server) {
      
      io.on('connection', (socket) => {
         socket.on('joinRoom', ({userId, otherUserId}) => {
+            console.log('joinRoom:', { userId, otherUserId });
             const room = [userId, otherUserId].sort().join('_');
             socket.join(room);
         });
         
         socket.on('sendMessage', async({from, to, content}) => {
+            console.log('sendMessage:', { from, to, content });
             const room = [from, to].sort().join('_');
             const message = await Message.create({from, to, content});
             io.to(room).emit('receiveMessage', message);
